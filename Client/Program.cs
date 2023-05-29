5eece693a5400c2e674fa8e40f22db1700b05c3d
CodekaUp <CodekaUp@users.noreply.github.com>
2023-05-29 09:11:28 +0300

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace Client
+{
+    //Код приложения его вызывающий
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                string filePath = Console.ReadLine();
+                string Url = "https://localhost:44355/sorting/post/wordcount";
+
+                using HttpClient httpClient= new HttpClient();
+                {
+                    HttpResponseMessage response = await httpClient.PostAsJsonAsync(Url, filePath);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Dictionary<string, int> wordCounts = await response.Content.ReadAsAsync<Dictionary<string, int>>();
+
+                        string outputFilePath = Path.Combine(Path.GetDirectoryName(filePath), "Output.txt");
+
+                        using (StreamWriter writer = new StreamWriter(outputFilePath))
+                        {
+                            foreach (KeyValuePair<string, int> v in wordCounts.OrderByDescending(x => x.Value))
+                            {
+                                writer.WriteLine($"{v.Key} - {v.Value}");
+                            }
+                        }
+                        Console.WriteLine($"Публичный метод сохранен в файл {outputFilePath}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Файл не содержит слов");
+                    }
+                    Console.ReadLine();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Sorting_WebApi/Controllers/SortingController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.PortableExecutable;
+using System.Text.RegularExpressions;
+
+namespace Sorting_WebApi.Controllers
+{
+    [Route("sorting")]
+    [ApiController]
+    public class SortingController : ControllerBase
+    {
+        //Web-Api сервис
+        [HttpPost, Route("post/wordcount")]
+        public async Task<ActionResult<Dictionary<string, int>>> GetWordCountsParallel([FromBody]string filePath)
+        {
+            try
+            {
+                Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string line;
+
+                    while ((line = await reader.ReadLineAsync()) != null)
+                    {
+                        Parallel.ForEach(Regex.Split(line.ToLower(), @"\W+"), word =>
+                        {
+                            if (!string.IsNullOrEmpty(word))
+                            {
+                                if (wordCounts.ContainsKey(word))
+                                {
+                                    wordCounts[word]++;
+                                }
+                                else
+                                {
+                                    wordCounts.Add(word, 1);
+                                }
+                            }
+                        });
+                    }
+                }
+                return wordCounts;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
